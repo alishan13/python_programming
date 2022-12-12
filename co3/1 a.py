afb0772d7e57fd83894c67705f7168c23d203659
ameen-18 <120323828+ameen-18@users.noreply.github.com>
2022-12-12 12:20:43 +0530

    Add files via upload

---
+++ co3/1 a.py
@@ -0,0 +1,7 @@
+import random
+l1=['ann','ben','ciril','Deril','eric','fugi']
+#print(random.choice(l1))
+#print(random.choice(l1,k=4))
+# print(random.choice(l1,k=1))
+random.shuffle(l1)
+print(l1)

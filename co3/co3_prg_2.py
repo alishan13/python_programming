1c8ffa6cc281eda29af94eac7740985b3149a99b
alishan13 <119841577+alishan13@users.noreply.github.com>
2022-12-14 15:14:01 +0530

    Update co3_prg_2.py

--- co3/co3_prg_2.py
+++ co3/co3_prg_2.py
@@ -5,13 +5,14 @@ b=int(input('Enter the second number : '))
 choice=int(input('Enter your choice 1.add 2.substract 3.multiply 4.divide : '))
 
 if choice == 1:
-    print(c.sum(a,b))
+    print(f"{a} + {b} = {c.sum(a,b)}")
 
 elif choice == 2:
-    print(c.sub(a,b))
+    print(f"{a} - {b} = {c.sub(a,b)}")
 
 elif choice == 3:
-    print(c.mul(a,b))
+    print(f"{a} * {b} = {c.mul(a,b)}")
 
 elif choice == 4:
-    print(c.div(a,b))
+    print(f"{a} / {b} = {c.div(a,b)}")
+

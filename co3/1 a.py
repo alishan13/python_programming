3ad4e35cac3f81dfc541002bfdc9f0e1bc609744
ameen-18 <120323828+ameen-18@users.noreply.github.com>
2023-02-15 12:18:23 +0530

    Delete co3 directory

--- co3/1 a.py
+++
@@ -1,7 +0,0 @@
-import random
-l1=['ann','ben','ciril','Deril','eric','fugi']
-#print(random.choice(l1))
-#print(random.choice(l1,k=4))
-# print(random.choice(l1,k=1))
-random.shuffle(l1)
-print(l1)
--- co3/1 e.py
+++
@@ -1,8 +0,0 @@
-import statistics as s
-l1=[20,30,40,50,60,70,100]
-print("mean:",s.mean(l1))
-print("median:",s.median(l1))
-print("mode:",s.mode(l1))
-print("Harmonc mean:",s.harmonic_mean(l1))
-print("statistics varience:",s.variance(l1))
-print("statistics Median low:",s.median_low([-12,6.6,-3,4,6,7.1,-9.22]))
